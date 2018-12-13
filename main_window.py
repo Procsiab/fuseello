5f5ad122e86e57ea07f97251d57a4685f767952a
Procsiab <lerokamut@gmail.com>
2018-12-13 20:36:12 +0100

    Changed naming convention and added message dialog on file overwrite

--- main_window.py
+++ main_window.py
@@ -3,14 +3,14 @@
 This module contains the main window callback handlers and global definitions
 '''
 
+from shutil import copy
+from time import time
 #pylint: disable=wrong-import-position
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 #pylint: enable=wrong-import-position
 from BoardList import BoardList
-from shutil import copy
-from time import time
 
 # Define global variables
 BOARDS_FILE = ""
@@ -30,19 +30,19 @@ class FuseelloHandler():
         global BOARDS_FILE
         global BOARDS
         # Get involved objects references
-        _PATH_LABEL = self.guiObjects.get_object("main_window-lbl_filePath")
-        _FILE_BUTTON = self.guiObjects.get_object("main-window-btn_chooseBoardsFile")
-        _COMBO_BOARDS = self.guiObjects.get_object("comboBox_boards_list")
+        pathLabel = self.guiObjects.get_object("main_window-lbl_filePath")
+        fileButton = self.guiObjects.get_object("main-window-btn_chooseBoardsFile")
+        comboBoards = self.guiObjects.get_object("comboBox_boards_list")
         # Obtain selected file name
-        BOARDS_FILE = _FILE_BUTTON.get_filename()
+        BOARDS_FILE = fileButton.get_filename()
         # Update the label text
-        _PATH_LABEL.set_text("  File path: " + BOARDS_FILE)
+        pathLabel.set_text("  File path: " + BOARDS_FILE)
         # Load the boards into a BoardList object
         BOARDS = BoardList(BOARDS_FILE)
         # Then add all the boards names to the ComboBox
-        _COMBO_BOARDS.remove_all()
+        comboBoards.remove_all()
         for board in BOARDS.getBoardNames():
-            _COMBO_BOARDS.append_text(board)
+            comboBoards.append_text(board)
 
     def on_comboBox_boards_list_changed(self, comboBox):
         '''
@@ -53,15 +53,29 @@ class FuseelloHandler():
         # Update selected board text
         SELECTED_BOARD = comboBox.get_active_text()
         # Get the selected board fuses
-        _LOW_FUSE, _HIGH_FUSE, _EXTENDED_FUSE = BOARDS.getBoardFuses(SELECTED_BOARD)
+        lowFuse, highFuse, extendedFuse = BOARDS.getBoardFuses(SELECTED_BOARD)
         # Get involved objects references
-        _ENTRY_LOW_FUSES = self.guiObjects.get_object("entry_low_fuses")
-        _ENTRY_HIGH_FUSES = self.guiObjects.get_object("entry_high_fuses")
-        _ENTRY_EXTENDED_FUSES = self.guiObjects.get_object("entry_extended_fuses")
+        entryLowFuse = self.guiObjects.get_object("entry_low_fuses")
+        entryHighFuse = self.guiObjects.get_object("entry_high_fuses")
+        entryExtendedFuse = self.guiObjects.get_object("entry_extended_fuses")
         # Set the text in the fuse entries
-        _ENTRY_LOW_FUSES.set_text(_LOW_FUSE[0])
-        _ENTRY_HIGH_FUSES.set_text(_HIGH_FUSE[0])
-        _ENTRY_EXTENDED_FUSES.set_text(_EXTENDED_FUSE[0])
+        entryLowFuse.set_text(lowFuse[0])
+        entryHighFuse.set_text(highFuse[0])
+        entryExtendedFuse.set_text(extendedFuse[0])
+
+    def __writeEntryValuesToFile__(self):
+        '''
+        Helper to save fuses values from the entries into the boards file
+        '''
+        # Get involved objects references
+        entryLowFuse = self.guiObjects.get_object("entry_low_fuses")
+        entryHighFuse = self.guiObjects.get_object("entry_high_fuses")
+        entryExtendedFuse = self.guiObjects.get_object("entry_extended_fuses")
+        # Create a tuple of fuse values from the entry boxes
+        entryFuses = (entryLowFuse.get_text(), entryHighFuse.get_text(),
+                        entryExtendedFuse.get_text())
+        # Write fuse values to file
+        BOARDS.setBoardFuses(SELECTED_BOARD, entryFuses)
 
     def on_btn_overwrite_clicked(self, button):
         '''
@@ -69,16 +83,21 @@ class FuseelloHandler():
         '''
         global BOARDS
         global SELECTED_BOARD
+        # If a board was selected from the dropdown
         if SELECTED_BOARD is not "":
-            # Get involved objects references
-            _ENTRY_LOW_FUSES = self.guiObjects.get_object("entry_low_fuses")
-            _ENTRY_HIGH_FUSES = self.guiObjects.get_object("entry_high_fuses")
-            _ENTRY_EXTENDED_FUSES = self.guiObjects.get_object("entry_extended_fuses")
-            # Create a tuple of fuse values from the entry boxes
-            _ENTRY_FUSES = (_ENTRY_LOW_FUSES.get_text(), _ENTRY_HIGH_FUSES.get_text(),
-                            _ENTRY_EXTENDED_FUSES.get_text())
-            # Write fuse values to file
-            BOARDS.setBoardFuses(SELECTED_BOARD, _ENTRY_FUSES)
+            # Define and show a warning dialog
+            warnDialog = Gtk.MessageDialog(parent=self.guiObjects.get_object("main_window"),
+                flags=0, message_type=Gtk.MessageType.WARNING,
+                buttons=Gtk.ButtonsType.OK_CANCEL,
+                text="You may loose your data!")
+            warnDialog.format_secondary_text(
+                "You clicked: " + button.get_label() + "\n"
+                + "Are you sure you want to overwrite the selected 'boards.txt' file?")
+            userChoice = warnDialog.run()
+            if userChoice == Gtk.ResponseType.OK:
+                self.__writeEntryValuesToFile__()
+            # Close the dialog
+            Gtk.Widget.destroy(warnDialog);
 
     def on_btn_save_copy_clicked(self, button):
         '''
@@ -86,23 +105,25 @@ class FuseelloHandler():
         '''
         global BOARDS_FILE
         global SELECTED_BOARD
+        # If a board was selected from the dropdown
         if SELECTED_BOARD is not "":
             # Get current time in milliseconds
             currMillis = str(int(round(time() * 1000)))
             # Copy the original file, appending the timestamp to its name
             copy(BOARDS_FILE, BOARDS_FILE + '_' + currMillis)
-            # Call the handler which will write the file
-            self.on_btn_overwrite_clicked(button)
+            # Write the original file
+            self.__writeEntryValuesToFile__()
+
 
 if __name__ == "__main__":
     # Load GLADE interface file
-    _BUILDER = Gtk.Builder()
-    _BUILDER.add_from_file("main_window.glade")
+    builder = Gtk.Builder()
+    builder.add_from_file("main_window.glade")
     # Create Instance of main window
-    _GUI_MAIN = _BUILDER.get_object("main_window")
-    _GUI_MAIN.connect("destroy", Gtk.main_quit)
-    _GUI_MAIN.show_all()
+    guiMain = builder.get_object("main_window")
+    guiMain.connect("destroy", Gtk.main_quit)
+    guiMain.show_all()
     # Connect handlers to GUI
-    _BUILDER.connect_signals(FuseelloHandler(_BUILDER))
+    builder.connect_signals(FuseelloHandler(builder))
     # Run GTK main loop
     Gtk.main()
